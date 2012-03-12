3ded6ba8c046ca11a034473aec10fdf5b902d3dd
azki <azki@azki.org>
2012-03-12 18:20:19 +0900

    initial commit

---
+++ proxyChanger/proxyChanger/Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace proxyChanger
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다.
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마십시오.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(12, 137);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(315, 21);
+            this.textBox1.TabIndex = 3;
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button2.Location = new System.Drawing.Point(12, 164);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(109, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "On";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // listBox1
+            // 
+            this.listBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 12;
+            this.listBox1.Location = new System.Drawing.Point(12, 46);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(315, 64);
+            this.listBox1.TabIndex = 5;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // button3
+            // 
+            this.button3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button3.Location = new System.Drawing.Point(137, 164);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(109, 23);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Off";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(81, 12);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Current state:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("굴림", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label2.Location = new System.Drawing.Point(10, 26);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 12);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "label2";
+            // 
+            // timer2
+            // 
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("굴림", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label3.Location = new System.Drawing.Point(10, 113);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(307, 11);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Preset from http://uie.daum.net/azki/proxyChanger.txt";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(339, 198);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Proxy Changer";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ proxyChanger/proxyChanger/Form1.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+using System.Runtime.InteropServices;
+using Microsoft.Win32;
+using System.Net;
+using System.IO;
+
+namespace proxyChanger
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        [DllImport("wininet.dll")]
+        public static extern bool InternetSetOption(IntPtr hInternet, int dwOption, IntPtr lpBuffer, int dwBufferLength);
+        public const int INTERNET_OPTION_SETTINGS_CHANGED = 39;
+        public const int INTERNET_OPTION_REFRESH = 37;
+        bool settingsReturn, refreshReturn;
+
+        private RegistryKey getRegKey()
+        {
+            return Registry.CurrentUser.OpenSubKey("Software\\Microsoft\\Windows\\CurrentVersion\\Internet Settings", true);
+        }
+        private void refreshIeSetting()
+        {
+            settingsReturn = InternetSetOption(IntPtr.Zero, INTERNET_OPTION_SETTINGS_CHANGED, IntPtr.Zero, 0);
+            refreshReturn = InternetSetOption(IntPtr.Zero, INTERNET_OPTION_REFRESH, IntPtr.Zero, 0);
+        }
+        private void addList(string server)
+        {
+            if (server != "")
+            {
+                if (listBox1.Items.Contains(server) == false)
+                {
+                    listBox1.Items.Add(server);
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string server;
+            try
+            {
+                RegistryKey rkApp = getRegKey();
+                int enable = 1;
+                server = textBox1.Text;
+                rkApp.SetValue("ProxyEnable", enable, RegistryValueKind.DWord);
+                rkApp.SetValue("ProxyServer", server, RegistryValueKind.String);
+                refreshIeSetting();
+                timer2_Tick(null, null);
+                button2.Text = "Complete..";
+            }
+            catch (Exception)
+            {
+                server = "";
+                button2.Text = "Error..";
+            }
+            timer1.Start();
+            addList(server);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                RegistryKey rkApp = getRegKey();
+                int enable = 0;
+                rkApp.SetValue("ProxyEnable", enable, RegistryValueKind.DWord);
+                refreshIeSetting();
+                timer2_Tick(null, null);
+                button3.Text = "Complete..";
+            }
+            catch (Exception)
+            {
+                button3.Text = "Error..";
+            }
+            timer1.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            button2.Text = "On";
+            button3.Text = "Off";
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            textBox1.Text = (string)listBox1.SelectedItem;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Icon = Properties.Resources.Icon1;
+
+            timer1.Interval = 2000;
+            timer2.Interval = 1000;
+            timer2.Start();
+            timer2_Tick(null, null);
+
+            getServerFromUie();
+        }
+        private void getServerFromUie()
+        {
+            try
+            {
+                WebClient wc = new WebClient();
+                byte[] data = wc.DownloadData("http://uie.daum.net/azki/proxyChanger.txt");
+                System.Text.UTF8Encoding encoder = new UTF8Encoding();
+                string serverText = encoder.GetString(data);
+                string[] arr = serverText.Split('\n');
+                for (int i = 0; i < arr.Length; i += 1)
+                {
+                    addList(arr[i].Trim());
+                }
+            }
+            catch (Exception) { }
+        }
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            try
+            {
+                RegistryKey rkApp = getRegKey();
+                int enable = (int)rkApp.GetValue("ProxyEnable", 0);
+                string server = (string)rkApp.GetValue("ProxyServer", "");
+                label2.Text = (enable != 0 ? "[On] " : "[Off] ") + server;
+            }
+            catch (Exception)
+            {
+                label2.Text = "Error..";
+            }
+        }
+
+    }
+}
